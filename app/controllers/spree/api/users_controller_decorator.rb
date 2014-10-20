56d6538c7253e492dd72714dd7b80ad7b4411644
Jaime Victoria <jaime.victoria@crowdint.com>
2014-10-20 16:42:57 -0500

    Add pry to the project

--- app/controllers/spree/api/users_controller_decorator.rb
+++ app/controllers/spree/api/users_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::Api::UsersController.class_eval do
     authorize! :create, Spree.user_class
     @user = Spree.user_class.new(user_params)
     if @user.save && @user.generate_spree_api_key!
-      retrieve_spree_key_attribute
+      retrieve_spree_api_key
       respond_with(@user, :status => 201, :default_template => :show)
     else
       invalid_resource!(@user)
@@ -11,7 +11,7 @@ Spree::Api::UsersController.class_eval do
   end
 
   private
-    def retrieve_spree_key_attribute
+    def retrieve_spree_api_key
       Spree::Api::ApiHelpers.user_attributes << 'spree_api_key'
     end
 
